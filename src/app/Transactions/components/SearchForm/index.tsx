769b5ea74b6fb98df1dc6e6015b5f51cf3013961
MARTINZ <lucasowza@hotmail.com>
2023-10-17 02:12:26 -0300

    feat: :sparkles: Create application DT-MONEY (first commit)

---
+++ src/app/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,52 @@
+import { MagnifyingGlass } from 'phosphor-react'
+import * as S from './styles'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
+import { useContextSelector } from 'use-context-selector'
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
+export function SearchForm() {
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchTransaction(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
+  }
+
+  return (
+    <S.SearchFormContainer onSubmit={handleSubmit(handleSearchTransaction)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20} />
+        Buscar
+      </button>
+    </S.SearchFormContainer>
+  )
+}
+
+// Example memo use for optimize
+// export const SearchForm = memo(SearchFormComponent)
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,111 @@
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import * as S from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
+import * as z from 'zod'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useContextSelector } from 'use-context-selector'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+
+const NewTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.coerce.number(),
+  category: z.string(),
+  type: z.enum(['income', 'outcome']),
+})
+
+type NewTransactionFormInputs = z.infer<typeof NewTransactionFormSchema>
+
+export function NewTransactionModal() {
+  const createTransaction = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.createTransaction
+    },
+  )
+
+  const {
+    control,
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting },
+  } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(NewTransactionFormSchema),
+    defaultValues: {
+      type: 'income',
+    },
+  })
+
+  async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+    const { description, price, category, type } = data
+    await createTransaction({
+      description,
+      price,
+      category,
+      type,
+    })
+
+    reset()
+  }
+
+  return (
+    <Dialog.Portal>
+      <S.Overlay />
+
+      <S.Content>
+        <Dialog.Title>Nova transação</Dialog.Title>
+        <S.CloseButton>
+          <X size={24} />
+        </S.CloseButton>
+
+        <form action="" onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input
+            {...register('description')}
+            type="text"
+            placeholder="Descrição"
+            required
+          />
+          <input
+            {...register('price')}
+            type="number"
+            placeholder="Preço"
+            required
+          />
+          <input
+            {...register('category')}
+            type="text"
+            placeholder="Categoria"
+            required
+          />
+
+          <Controller
+            control={control}
+            name="type"
+            render={({ field }) => {
+              return (
+                <S.TransactionType
+                  onValueChange={field.onChange}
+                  value={field.value}
+                >
+                  <S.TransactionTypeButton value="income" variant="income">
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                  </S.TransactionTypeButton>
+                  <S.TransactionTypeButton value="outcome" variant="outcome">
+                    <ArrowCircleDown size={24} />
+                    Saída
+                  </S.TransactionTypeButton>
+                </S.TransactionType>
+              )
+            }}
+          />
+
+          <button type="submit" disabled={isSubmitting}>
+            Cadastrar
+          </button>
+        </form>
+      </S.Content>
+    </Dialog.Portal>
+  )
+}
